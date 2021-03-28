f49c148717cfecb78578230945615714de0ae27c
Shawn Yu <55592708+shawnyu5@users.noreply.github.com>
2021-03-27 22:44:21 -0400

    Update oop_submission.sh

--- oop_submission.sh
+++ oop_submission.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-#purpose of this file: generate a oop submission command
-#Date: 2021-03-27 
-#---------------------------------
-
-#~profname.proflastname/submit 2??/w#/p1
 function submission
 {
     read -p "what is the prof's first name? " FIRSTNAME
@@ -11,7 +5,6 @@ function submission
     read -p "what course section is this? " SECTION
     read -p "which workshop are you submitting? " WS
     read -p "Which part of the workshop are you submitting? " PART
-
     echo "your submission command is: "
     echo "~$FIRSTNAME.$LASTNAME/submit $SECTION/w$WS/p$PART"
 }
